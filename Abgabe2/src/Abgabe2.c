3f42344e83889d677452ab8e89b069b130aca9e8
student <student@ubuntu>
2022-11-08 08:57:20 -0800

    Test Abgabe2

--- Abgabe2/src/Abgabe2.c
+++ Abgabe2/src/Abgabe2.c
@@ -1,17 +1,7 @@
-/*
- ============================================================================
- Name        : Abgabe2.c
- Author      : StudentT
- Version     :
- Copyright   : Hochschule Augsburg
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Abgabe2"); /* prints !!!Hello World!!! */
+	printf("!!!Abgabe2");
 	return EXIT_SUCCESS;
 }
